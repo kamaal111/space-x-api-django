2a10a8a3dc985da374a51bdb73a5574863b17457
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-21 14:26:08 +0200

    Graphql quries does not throw errors with a POST request

--- api/urls.py
+++ api/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+
 from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('spacex.urls')),
-    path('graphql', GraphQLView.as_view(graphiql=True)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('gql', csrf_exempt(GraphQLView.as_view(batch=True))),
 ]
